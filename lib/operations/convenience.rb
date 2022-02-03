f549a535de69213696e83929949d294654f03ea9
Arkadiy Zabazhanov <kinwizard@gmail.com>
2022-02-03 13:31:32 +0300

    Add bang proxy to Convenience

--- lib/operations/convenience.rb
+++ lib/operations/convenience.rb
@@ -28,11 +28,52 @@
 #
 # @see Operations::Command.build
 #
+# Also, if this class is used as a container to cache the command
+# instance under some name, this module will provide a method missing
+# to call the command with `#call!` method using the `#call` method
+# as an interface.
+#
+# @example
+#
+#   class Namespace::OperationName
+#     extend Operations::Convenience
+#
+#     def self.default
+#       Operations::Command.new(...)
+#     end
+#   end
+#
+#   # A normall command call
+#   Namespace::OperationName.default.call(...)
+#   # Raises exception in case of failure
+#   Namespace::OperationName.default.call!(...)
+#   # Acts exactly the same way as the previous one
+#   # but notice where the bang is.
+#   Namespace::OperationName.default!.call(...)
+#
+# This is especially convenient when you have a DSL that
+# expects some object responding to `#call` method but you want
+# to raise an exception. In this case you would just pass
+# `Namespace::OperationName.default!` into it.
+#
 module Operations::Convenience
   def self.extended(mod)
     mod.include Dry::Monads[:result]
     mod.include Dry::Monads::Do.for(:call)
     mod.extend Dry::Initializer
+  end
+
+  def method_missing(name, *args, **kwargs, &block)
+    name_without_suffix = name.to_s.delete_suffix("!").to_sym
+    if name.to_s.end_with?("!") && respond_to?(name_without_suffix)
+      public_send(name_without_suffix, *args, **kwargs, &block).method(:call!)
+    else
+      super
+    end
+  end
+
+  def respond_to_missing?(name, *)
+    (name.to_s.end_with?("!") && respond_to?(name.to_s.delete_suffix("!").to_sym)) || super
   end
 
   def contract(prefix = nil, from: OperationContract, &block)
