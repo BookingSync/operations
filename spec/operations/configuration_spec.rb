3205ced161ab2074cb11ca09a5712c8cf176b148
Arkadiy Zabazhanov <kinwizard@gmail.com>
2022-03-24 14:45:44 +0600

    Fix rubocop

--- spec/operations/configuration_spec.rb
+++ spec/operations/configuration_spec.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
 RSpec.describe Operations::Configuration do
-  subject(:configuration) { Operations::Configuration.new(**options) }
+  subject(:configuration) { described_class.new(**options) }
 
-  let(:error_reporter) { ->() {} }
-  let(:transaction) { ->() {} }
+  let(:error_reporter) { -> {} }
+  let(:transaction) { -> {} }
   let(:options) { { error_reporter: error_reporter, transaction: transaction } }
 
-  describe '#to_h' do
+  describe "#to_h" do
     subject(:to_h) { configuration.to_h }
 
     it { is_expected.to eq(error_reporter: error_reporter, transaction: transaction) }
