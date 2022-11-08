8ec1ca43d498415ab132682c7e0b25f658ce37fe
Radek <radek_zawada@op.pl>
2022-11-08 10:58:55 +0100

    On_success inherits from base class

--- lib/operations/components/on_success.rb
+++ lib/operations/components/on_success.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require "operations/components/base"
+require "operations/components/base_callback"
 
 # This component handles `on_success:` callbacks passed to the
 # composite. Every `on_success:` entry is called in a separate
@@ -8,48 +8,5 @@ require "operations/components/base"
 # result of the whole operation is not affected.
 # If there is a failure in any entry, it is reported with
 # `error_reporter` proc.
-class Operations::Components::OnSuccess < Operations::Components::Base
-  include Dry::Monads::Do.for(:call_entry)
-
-  param :callable, type: Operations::Types::Array.of(Operations::Types.Interface(:call))
-
-  def call(params, context)
-    on_success_results = callable.map do |entry|
-      call_entry(entry, params, **context)
-    end
-
-    maybe_report_failure(result(
-      component: :operation,
-      params: params,
-      context: context,
-      on_success: on_success_results
-    ))
-  end
-
-  private
-
-  def call_entry(entry, params, **context)
-    args = call_args(entry, types: %i[req opt])
-
-    result = transaction.call do
-      yield(args.one? ? entry.call(params, **context) : entry.call(**context))
-    end
-
-    Success(result)
-  rescue Dry::Monads::Do::Halt => e
-    e.result
-  rescue => e
-    Failure(e)
-  end
-
-  def maybe_report_failure(result)
-    if result.on_success.any?(Failure)
-      error_reporter&.call(
-        "Operation on_success side-effects went sideways",
-        result: result.as_json
-      )
-    end
-
-    result
-  end
+class Operations::Components::OnSuccess < Operations::Components::BaseCallback
 end
